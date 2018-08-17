5b2f1fb91ff187f71d9a92378d79dfadf99ad12f
geekett81301 <41696694+geekett81301@users.noreply.github.com>
2018-08-17 10:17:23 -0600

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,41 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+## Creates an object that can caches a passed matrix and the inverse of passed matrix
+## Matrices must be square
+makeCacheMatrix <- function(regularM = matrix()) {
+        inverseM <- NULL
+        ## reset with new matrix
+        set <- function(y) {
+                regularM <<- y
+                inverseM <<- NULL
+        }
+        ## get matrix
+        get <- function() regularM
+        ## cache inverse matrix
+        setinverse <- function(receiveInverse) inverseM <<- receiveInverse
+        ## retrieve cached inverse matrix
+        getinverse <- function() inverseM
+        ## return object
+        list(set=set, get=get, setinverse=setinverse,getinverse=getinverse)
 }
 
-
-## Write a short comment describing this function
+## Computes inverse of matrix returned by makeCacheMatrix or returns cached inverse
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        ## check for invertible matrix
+        if (!class(try(solve(x$get()),silent=T))=="matrix") {
+                message("Matrix has no inverse!")
+                return()
+        }
+        ## retrieve cached inverse
+        cachedM <- x$getinverse()
+        ## if inverse already calculated, return cached inverse
+        if (!is.null(cachedM)) {
+                        message("Retrieving cached inverse...")
+                        return(cachedM)
+        }
+        ## calculate and set inverse
+        origM <- x$get()
+        newInverse <- solve(origM)
+        x$setinverse(newInverse)
+        ## Return a matrix that is the inverse of regularM
+        message(newInverse)
 }
